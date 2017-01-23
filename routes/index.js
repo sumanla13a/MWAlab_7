524bf44a8ff80f887346adafa6c30a3ae0bca11f
sumanla13a <lamasuman2@gmail.com>
2017-01-23 13:49:31 -0600

    lab 7

---
+++ routes/index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const crypto = require('crypto');
+
+const db = require(path.join(global.appRoot, './model'));
+const homework6 = db.homework6;
+/* GET home page. */
+const controllers = {
+	getMessage: function(req, res, next) {
+		homework6.findOne({}, function(err, rest) {
+			if(err) {
+				return next(err);
+			}
+			req.data = rest;
+			next();
+	  		// return res.render('index', { title: rest.message });
+		});
+	},
+	decipherMessage: function(req, res, next) {
+		const decipher = crypto.createDecipher('aes256', 'asaadsaad');
+		req.decipheredMessage = '';
+		decipher.on('readable', () => {
+			var data = decipher.read();
+			if(data) {
+				req.decipheredMessage += data.toString('utf8');
+			}
+			console.log(req.decipheredMessage);
+		});
+		decipher.on('end', function() {
+			console.log(arguments);
+			next();
+		});
+		decipher.write(req.data.message, 'hex');
+		decipher.end();
+	},
+	renderView: function(req, res) {
+		return res.render('index', { title: req.decipheredMessage });
+	}
+};
+router.get('/', controllers.getMessage, controllers.decipherMessage, controllers.renderView);
+
+module.exports = router;
